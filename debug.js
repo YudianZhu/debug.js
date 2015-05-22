3cafe0d0abac40e3bfccd937b99dfde4e9458869
sherry <ccqgithub@users.noreply.github.com>
2015-05-22 11:52:34 +0800

    Update debug.js

--- debug.js
+++ debug.js
@@ -22,7 +22,7 @@
 
 		$clear.className = 'sdebug-clear';
 		$clear.innerHTML = 'C';
-		$con.appendChild($clear);
+		$box.appendChild($clear);
 
 		$con.className = 'sdebug-con';
 		$box.appendChild($con);
@@ -101,7 +101,7 @@
 
 	function createStyle() {
 		var $style = document.createElement('style'),
-			css = '.sdebug,.sdebug *,.sdebug *:before,.sdebug *:after{padding: 0; margin: 0; box-sizing: border-box;}.sdebug{position: fixed; width: 100%; height: 0; left: 0; top: 0; font-size: 12px; font-family: "Microsoft Yahei"; line-height: 1.5; z-index: 999999;}.sdebug-ctrl{position: absolute; width: 20px; height: 20px; text-align: center; line-height: 20px; left: 0; top: 0; z-index: 2; background: green; border-radius: 50px; color: #fff; cursor: pointer;}.sdebug-clear{position: absolute; width: 20px; height: 20px; text-align: center; line-height: 20px; right: 0; top: 0; z-index: 2; background: #ddd; border-radius: 50px; color: #fff; cursor: pointer;}.sdebug-con{position: absolute; left: 0; right: 0; top: 0; bottom: 0; padding: 20px; display: none; background: #000; overflow: auto; z-index: 1;}.sdebug-show{height: 100%;}.sdebug-show .sdebug-con{display: block;}.sdebug-pre{padding: 5px 0; color: #fff; border-bottom: 1px solid #ddd; white-space: pre-wrap;}.sdebug-log{color: #fff;}.sdebug-info{color: #3F7C3F;}.sdebug-warn{color: #E69842;}.sdebug-error{color: #C11142;}';
+			css = '.sdebug,.sdebug *,.sdebug *:before,.sdebug *:after{padding: 0; margin: 0; box-sizing: border-box;}.sdebug{position: fixed; width: 100%; height: 0; left: 0; top: 0; font-size: 12px; font-family: "Microsoft Yahei"; line-height: 1.5; z-index: 999999;}.sdebug-ctrl{position: absolute; width: 20px; height: 20px; text-align: center; line-height: 20px; left: 0; top: 0; z-index: 2; background: green; border-radius: 50px; color: #fff; cursor: pointer;}.sdebug-clear{position: absolute; width: 20px; height: 20px; text-align: center; line-height: 20px; right: 0; top: 0; z-index: 2; background: #ddd; border-radius: 50px; color: #fff; cursor: pointer; display: none;}.sdebug-con{position: absolute; left: 0; right: 0; top: 0; bottom: 0; padding: 20px; display: none; background: #000; overflow: auto; z-index: 1;}.sdebug-show{height: 100%;}.sdebug-show .sdebug-con, .sdebug-show .sdebug-clear{display: block;}.sdebug-pre{padding: 5px 0; color: #fff; border-bottom: 1px solid #ddd; white-space: pre-wrap;}.sdebug-log{color: #fff;}.sdebug-info{color: #3F7C3F;}.sdebug-warn{color: #E69842;}.sdebug-error{color: #C11142;}';
 
 		if($style.styleSheet){       
 			$style.styleSheet.cssText = css;  
